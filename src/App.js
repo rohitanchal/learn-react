7dbf46fa97cb6c0a0ce06dbfb348ab698689267a
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-11-03 13:47:53 +0530

    complete Prosp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import User from './User';
 import Header from './Header';
+import Student from './Student';
 
 function App() {
 
@@ -13,12 +14,18 @@ function App() {
 
       <Header />
       <User />
+      <Student name={" Rohit"} email={"rohit@gmail.com"} other={{address: 'Lucknow', mobile: '00000'}} />
+      <Student name={" Anchal"} email={"anchal@gmail.com"} other={{address: 'Lucknow', mobile: '111111'}} />
+      <Student name={" Rajan"} email={"rajan@gmail.com"} other={{address: 'Mau', mobile: '222222'}} />
+      <Student name={" Utkarsh"} email={"utkarsh@gmail.com"} other={{address: 'Bulandshehar', mobile: '333333'}} />
+
 
       <button onClick={() => alert("Hello")}>Click Me</button><hr />
       <div>
         <button onClick={alertmsg}>Click Me</button> {/*called alert without arrow function*/}<hr />
         <button onClick={() => alertmsg()}>Click Me</button> {/*called alert with arrow function*/}<hr />
       </div>
+
 
      
     </div>
---
+++ src/Student.js
@@ -0,0 +1,22 @@
+function Student(props) {
+    //here props called
+
+    console.warn(props);
+    return (
+        <div style={{ backgroundColor: "skyblue", margin: 10 }}>
+            <div> 
+                <hr />
+                <h1>Student Component</h1>
+            </div>
+            <div>
+                <hr />
+                <h1>Name :{props.name}</h1> {/*here props accessed with parameter*/}
+                <h2>Email : {props.email}</h2>
+                <h3>Address : {props.other.address}</h3>
+                <h3>Mobile : {props.other.mobile}</h3>
+            </div>
+            <hr />
+        </div>
+    );
+}
+export default Student;
