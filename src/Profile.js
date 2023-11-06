2feb3ebf894d364e36d57e4e6f416881b2e93346
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-11-06 16:43:49 +0530

    with option in lecture 18

--- src/Profile.js
+++ src/Profile.js
@@ -26,14 +26,21 @@ import React, { useState } from "react";
 
 function Profile(){
 
-    const [login, setLogin] = useState(true);
+    // const [login, setLogin] = useState(true);
+    const [login, setLogin] = useState(1);
 
     return(
         <div>
             {
-                login?
-                <h1>Your are login</h1>
-                :<h1>Login first</h1>
+                // login?
+                // <h1>Your are login</h1>
+                // :<h1>Login first</h1>
+
+                login==1?
+                <h1>Your option is 1 : user:1</h1>
+                :login==2?
+                <h2>Your option is 2 : user:2</h2>
+                :<h3>Your option is 2 : user:3</h3>
             }
         </div>
     )
